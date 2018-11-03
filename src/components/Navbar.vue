eb24d4891c730d8fd6f1fd1189ebac99c4ea5fea
Richard Trujillo Torres <richard@desemax.com>
2018-11-03 23:49:07 +0100

    Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+              <router-link to="/home" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Expenses
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+
+              <router-link to="/expenses" class="dropdown-item">Listing</router-link>
+              <router-link to="/expenses/create" class="dropdown-item">Add</router-link>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Statistics</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+

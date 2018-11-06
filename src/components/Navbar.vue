cab8f8f4740529f8a2ceff340caf59e25e21ca01
Richard Trujillo Torres <richard@desemax.com>
2018-11-06 12:18:28 +0100

    Update navbar links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="navbar">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <router-link to="/" class="navbar-brand">Home</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-              <router-link to="/home" class="nav-link">Home</router-link>
-          </li>
           <li class="nav-item">
             <router-link to="/about" class="nav-link">About</router-link>
           </li>
@@ -30,9 +27,11 @@
               <a class="dropdown-item" href="#">Statistics</a>
             </div>
           </li>
+          <!--
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Disabled</a>
           </li>
+          -->
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">

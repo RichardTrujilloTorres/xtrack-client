4822abf367330322f94c59023d96d585828d0259
Richard Trujillo Torres <richard@desemax.com>
2018-11-06 12:19:27 +0100

    Add filters to app

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import filters from './filters'
 
 import popper from 'popper.js'
 import jquery from 'jquery'

392659247c4477cbc196d0dcb088a2af877b58a2
Richard Trujillo Torres <richard@desemax.com>
2018-11-06 12:16:16 +0100

    Add page not found component

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page-not-found">
+    <h1>Not Found</h1>
+    <h5>
+      <span>
+        Could not find the page you are looking for.
+      </span>
+    </h5>
+  </div>
+</template>

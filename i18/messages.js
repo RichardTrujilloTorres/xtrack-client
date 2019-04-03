201d1f1ffc24334427e6073ce9fbe0789f4ed46d
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-03 21:03:36 +0200

    Add internalization support

---
+++ i18/messages.js
@@ -0,0 +1,9 @@
+import en from './en'
+import it from './it'
+
+const messages = {
+    en: en,
+    it: it
+}
+
+export default messages
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import filters from './filters'
+import messages from '../i18/messages'
 
 // TODO find a better way to do this
 import popper from 'popper.js'
@@ -10,10 +11,26 @@ import jquery from 'jquery'
 import bootstrap from 'bootstrap/dist/js/bootstrap'
 import bootstrapCss from 'bootstrap/dist/css/bootstrap.css'
 
+const _ = require('lodash')
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import VueI18n from 'vue-i18n';
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueSweetalert2)
+Vue.use(VueI18n)
+
+
+// Internalization
+const i18n = new VueI18n({
+  locale: 'en',
+  messages
+})
+
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)

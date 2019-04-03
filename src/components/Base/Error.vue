582011eeb6a9c6e5c0b128f583be6b5f5ba09c5b
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-04 00:16:11 +0200

    Add general purpose error component

---
+++ src/components/Base/Error.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="card">
+            <div v-if="errors.length" class="alert alert-danger  alert-dismissible fade show" role="alert">
+                <button @click.prevent="clear" type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Error',
+        props: {
+            errors: {
+                type: Array,
+                required: false,
+                default: () => []
+            }
+        },
+        data() {
+            return {
+            }
+        },
+        methods: {
+            clear() {
+                this.errors.splice(0, 1)
+            }
+        }
+    }
+</script>

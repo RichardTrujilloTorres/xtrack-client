e609c2a4b7a140f2af24a1dda21b70eebf5c5467
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-04 04:43:02 +0200

    Add expense resource

---
+++ src/api/expense.js
@@ -0,0 +1,10 @@
+import Resource from "./resource";
+
+/**
+ * Represent the Expense resource.
+ *
+ * @type {Resource}
+ */
+const Expense = new Resource(`http://localhost:8000/api/expenses`)
+
+export default Expense

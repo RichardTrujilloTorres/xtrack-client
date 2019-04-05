f91bd11832bae9fed8f2c0c9ca9bcae82c686db8
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-05 07:50:27 +0200

    Add fields and cleanup

--- src/components/Expenses/Create.vue
+++ src/components/Expenses/Create.vue
@@ -1,103 +1,127 @@
 <template>
   <div>
     <div class="card">
-        <error :errors="errors"></error>
+      <vue-form
+        :state="formstate"
+        @submit.prevent="submit"
+      >
+        <div class="card-header">
+          {{ $t('expenses.create') }}
+        </div>
 
-            <vue-form :state="formstate" @submit.prevent="submit">
+        <!-- denomination -->
+        <div class="card card-body">
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <validate
+                tag="label"
+                class="form-group required-field"
+              >
+                <label>{{ $t('expenses.denomination') }}*</label>
+                <input
+                  v-model="model.denomination"
+                  class="form-control"
+                  type="text"
+                  name="denomination"
+                  required
+                >
 
-                <div class="card-header">
-                    Expense Creation
-                </div>
+                <field-messages
+                  show="$touched || $submitted"
+                  class="form-control-feedback"
+                >
+                  <div slot="required">
+                    {{ $t('fields.required') }}
+                  </div>
+                </field-messages>
+              </validate>
+            </div>
+          </div>
 
-                <!-- denomination -->
-                <div class="card card-body">
-                    <div class="form-row">
-                        <div class="col-md-4 mb-3">
-                            <validate tag="label" class="form-group required-field" >
-                                <label>Denomination *</label>
-                                <input class="form-control" v-model="model.denomination" type="text" name="denomination" required />
+          <!-- description -->
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <validate
+                tag="label"
+                class="form-group required-field"
+              >
+                <label>{{ $t('expenses.description') }}</label>
+                <input
+                  v-model="model.description"
+                  class="form-control"
+                  name="description"
+                  type="text"
+                  required
+                >
 
-                                <field-messages  show="$touched || $submitted" class="form-control-feedback">
-                                    <div slot="required">Denomination required</div>
-                                </field-messages>
-                            </validate>
+                <field-messages
+                  show="$touched || $submitted"
+                  class="form-control-feedback"
+                >
+                  <div slot="required">
+                    {{ $t('fields.required') }}
+                  </div>
+                  <div slot="description">
+                    {{ $t('fields.invalid') }}
+                  </div>
+                </field-messages>
+              </validate>
+            </div>
+          </div>
+        </div>
 
-                        </div>
-                    </div>
-
-                    <!-- description -->
-                    <div class="form-row">
-                        <div class="col-md-4 mb-3">
-                            <validate tag="label" class="form-group required-field" >
-                                <span>Description</span>
-                                <input class="form-control" v-model="model.description" name="description" type="text" required />
-
-                                <field-messages show="$touched || $submitted" class="form-control-feedback">
-                                    <div slot="required">Description is required</div>
-                                    <div slot="description">Invalid description</div>
-                                </field-messages>
-                            </validate>
-                        </div>
-                    </div>
-
-                </div>
-
-                <button class="btn btn-primary" type="submit">Save</button>
-            </vue-form>
+        <button
+          class="btn btn-primary"
+          type="submit"
+        >
+          {{ $t('actions.save') }}
+        </button>
+      </vue-form>
     </div>
   </div>
 </template>
 
 <script>
-    import api from '@/api'
-    import formMixins from "../../utils/formMixins";
-    import Error from "@/components/Base/Error";
-    import Expense from '@/api/expense'
+import formMixins from "../../utils/formMixins";
+import Expense from '@/api/expense'
 
 
-    export default {
+export default {
   name: 'Create',
-    components: {Error},
-    mixins: [formMixins],
-    data() {
-      return {
-          resource: Expense,
-          formstate: {},
-          model: {
-              denomination: '',
-              description: '...'
-          },
-          errors: [],
-          expense: {
-              denomination: null,
-              description: null
-          }
-      }
-    },
-    methods: {
-      onSuccess(res) {
-          this.$swal('Success', 'Expense successfully created', 'success')
-
-          this.$router.push('/expenses')
-      },
-          onFailure(res) {
-              this.$swal('Error', 'Could not create expense', 'error')
-
-              console.log(res)
-          },
-      submit() {
-          if (this.formstate.$invalid) {
-              this.$swal('Error', 'Please correct the errors before submitting', 'error')
-              return;
-          }
-
-
-          this.resource.store(this.model)
-              .then(res => this.onSuccess(res))
-              .catch(res => this.onFailure(res))
-
+  mixins: [formMixins],
+  data() {
+    return {
+      resource: Expense,
+      formstate: {},
+      model: {
+        denomination: '',
+        description: '...'
       }
     }
+  },
+  methods: {
+    onSuccess(res) {
+      this.$swal('Success', 'Expense successfully created', 'success')
+
+      this.$router.push('/expenses')
+    },
+    onFailure(res) {
+      this.$swal('Error', 'Could not create expense', 'error')
+
+      console.log(res)
+    },
+    submit() {
+      if (this.formstate.$invalid) {
+        this.$swal('Error', 'Please correct the errors before submitting', 'error')
+        return
+      }
+
+
+      this.resource.store(this.model)
+        .then(res => this.onSuccess(res))
+        .catch(res => this.onFailure(res))
+    }
+  }
 }
 </script>
 

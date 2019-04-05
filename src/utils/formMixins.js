bdd658f5905f5624b7e5b2cf73785dbf20f4cb58
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-05 07:37:20 +0200

    Add default success/error form alert procedures

--- src/utils/formMixins.js
+++ src/utils/formMixins.js
@@ -9,7 +9,22 @@ let formMixins = {
             if (!expense.denomination) {
                 errors.push('Denomination required')
             }
-        }
+        },
+        onSuccess(res) {
+            this.$swal('Success', 'Operation successfully completed', 'success')
+
+            this.$router.push('/')
+        },
+        onFailure(res) {
+            let message = 'Error while attempting to perform the operation'
+            if (res.hasOwnProperty('message')) {
+                message = res.message
+            }
+
+            this.$swal('Error', message, 'error')
+            console.log(res)
+        },
+
     }
 }
 

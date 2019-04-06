7e1f0b1de42ed26ec23dba5dcf59eb18ea7bb66b
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-06 02:58:33 +0200

    Add base delete confirmation (swal) modal

---
+++ src/components/Base/DeleteConfirmationModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button
+      class="btn btn-danger"
+      @click.prevent="showConfirmationModal"
+    >
+      {{ $t('actions.delete') }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeleteConfirmationModal',
+  props: {
+    callback: {
+      type: Function,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    showConfirmationModal() {
+      this.$swal.fire({
+        title: this.$i18n.t('actions.confirm'),
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: this.$i18n.t('actions.delete'),
+        cancelButtonText: this.$i18n.t('actions.cancel'),
+      }).then((result) => {
+        if (result.value) {
+          this.callback(this.id)
+        }
+      })
+    }
+  }
+}
+</script>

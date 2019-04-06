671b3477d4da995b74a30e8f4da2cec74858802e
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-06 23:59:22 +0200

    Update edit view

--- src/components/Expenses/Edit.vue
+++ src/components/Expenses/Edit.vue
@@ -1,83 +1,208 @@
 <template>
   <div>
-    <div class="card">
-        <div v-if="errors.length" class="alert alert-danger" role="alert">
-            <ul>
-                <li v-for="error in errors">{{ error }}</li>
-            </ul>
+    <!-- TODO replace w/ the spinner -->
+    <span v-if="isLoading">... loading</span>
+
+
+    <div
+      v-if="!isLoading"
+      class="card"
+    >
+      <vue-form
+        :state="formstate"
+        @submit.prevent="submit"
+      >
+        <div class="card-header">
+          {{ $t('expenses.edit') }}
         </div>
 
-        <form @submit.prevent="submit">
+        <!-- denomination -->
+        <div class="card card-body">
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <validate
+                tag="label"
+                class="form-group required-field"
+              >
+                <label>{{ $t('expenses.denomination') }}*</label>
+                <input
+                  v-model="model.denomination"
+                  class="form-control"
+                  type="text"
+                  name="denomination"
+                  required
+                >
 
-          <div class="card-header">
-              <input v-model="expense.denomination" class="form-control" />
+                <field-messages
+                  show="$touched || $submitted"
+                  class="form-control-feedback"
+                >
+                  <div slot="required">
+                    {{ $t('fields.required') }}
+                  </div>
+                </field-messages>
+              </validate>
+            </div>
           </div>
 
-          <div class="card card-body">
-              <textarea v-model="expense.description" class="form-control"></textarea>
+          <!-- description -->
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <validate
+                tag="label"
+                class="form-group required-field"
+              >
+                <label>{{ $t('expenses.description') }}</label>
+                <input
+                  v-model="model.description"
+                  class="form-control"
+                  name="description"
+                  type="text"
+                  required
+                >
+
+                <field-messages
+                  show="$touched || $submitted"
+                  class="form-control-feedback"
+                >
+                  <div slot="required">
+                    {{ $t('fields.required') }}
+                  </div>
+                  <div slot="description">
+                    {{ $t('fields.invalid') }}
+                  </div>
+                </field-messages>
+              </validate>
+            </div>
           </div>
 
-            <button class="btn btn-primary" type="submit">Save</button>
-            <button class="btn btn-default" type="reset">Reset</button>
-        </form>
+          <!-- category -->
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <validate
+                tag="label"
+                class="form-group required-field"
+              >
+                <label class="type__label">{{ $t('expenses.category') }}</label>
+                <multiselect
+                  v-model="model.category"
+                  multi-select-validator
+                  name="category"
+                  :options="categories"
+                  :searchable="false"
+                  :close-on-select="true"
+                  :show-labels="true"
+                  :placeholder="$t('requests.select', { selection: 'category' })"
+                  :deselect-label="''"
+                  :selected-label="''"
+                  required
+                />
+
+                <field-messages
+                  show="$touched || $submitted"
+                  class="form-control-feedback"
+                >
+                  <div slot="required">
+                    {{ $t('fields.required') }}
+                  </div>
+                  <div slot="category">
+                    {{ $t('fields.invalid') }}
+                  </div>
+                </field-messages>
+              </validate>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <button
+          class="btn btn-primary"
+          type="submit"
+        >
+          {{ $t('actions.save') }}
+        </button>
+      </vue-form>
     </div>
   </div>
 </template>
 
 <script>
-    import api from '@/api'
-    import formMixins from "../../utils/formMixins";
-
-    export default {
-        created() {
-            this.id = this.$route.params.id
-            this.loadExpense()
-        },
+import formMixins from "../../utils/formMixins";
+import Expense from '@/api/expense'
+import Multiselect from 'vue-multiselect';
+import {mapGetters} from 'vuex';
 
 
-  name: 'Edit',
-    mixins: [formMixins],
-    data() {
-      return {
-          id: null,
-          errors: [],
-          expense: {
-              denomination: null,
-              description: null
-          }
-      }
-    },
-    methods: {
-            loadExpense() {
-                api.get(`/expenses/${this.id}`)
-                    .then(res => {
-                        console.log(res)
-                        console.log(res.data.data)
-                        this.expense = res.data.data.expenses
-                    })
-                    .catch(res => {
-                        console.log(res)
-                    })
-            },
-
-      submit() {
-          this.validate(this.expense, this.errors)
-          if (this.errors.length) {
-              return
-          }
-
-          api.put(`/expenses/${this.id}`, this.expense)
-              .then(res => {
-                  console.log(res)
-                  this.$swal('Success', 'Expense successfully created', 'success')
-                  this.$router.push('/expenses')
-              })
-              .catch(res => {
-                  console.log(res)
-                  this.$swal('Error', 'Could not create expense', 'error')
-              })
-      }
+export default {
+  name: 'Create',
+  components: {Multiselect},
+  mixins: [formMixins],
+  data() {
+    return {
+      isLoading: true,
+      resource: Expense,
+      formstate: {},
+      model: {
+        denomination: '',
+        description: '...',
+        category: null
+      },
+      // TODO BE based
+      categories: [
+        'groceries',
+        'medicines',
+        'transportation',
+        'other'
+      ],
     }
+  },
+  computed: {
+    ...mapGetters(['getExpense']),
+    id() {
+      return this.$route.params.id
+    }
+  },
+  mounted() {
+    this.$store.dispatch('getExpense', this.id)
+      .then(() => {
+          // wait a bit to test the laoading
+        setTimeout(() => {
+          console.log('waiting')
+          this.isLoading = false
+        }, 3000)
+
+        this.model = {...this.getExpense}
+      })
+  },
+  methods: {
+    submit() {
+      if (this.formstate.$invalid) {
+        this.$swal('Error', this.$i18n.t('form.toCorrect'), 'error')
+        return
+      }
+
+      this.resource.update(this.model.id, this.model)
+        .then(res => this.onSuccess(res))
+        .catch(res => this.onFailure(res))
+    }
+  },
 }
 </script>
+<style>
+  .multiselect {
+    width: fit-content !important;
+  }
+
+  .multiselect__content-wrapper {
+    width: fit-content !important;
+  }
+
+  /*
+  .multiselect__element {
+    width: fit-content !important;
+  }
+  */
+</style>
 

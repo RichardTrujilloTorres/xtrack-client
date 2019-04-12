8e84dd1063e8e65d5fe924e7bef3b1f317348631
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-12 21:59:02 +0200

    Add user state handling

--- src/store.js
+++ src/store.js
@@ -1,49 +1,61 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Expense from "./api/expense";
+import {vueAuth} from "./main";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-      expenses: [],
-      expense: null,
-      resource: Expense
+    expenses: [],
+    expense: null,
+    resource: Expense,
+    user: {
+      email: ''
+    }
   },
   mutations: {
-      // sets the working expense
-      setExpense(state, expense) {
-          state.expense = expense
-      },
-      setExpenses(state, expenses) {
-          state.expenses = expenses
-      }
+    setUser(state, {email}) {
+      state.user.email = email
+    },
+
+    // sets the working expense
+    setExpense(state, expense) {
+      state.expense = expense
+    },
+    setExpenses(state, expenses) {
+      state.expenses = expenses
+    }
   },
   actions: {
-      // retrieves the working expense
-      getExpense(context, id) {
-          return this.state.resource.show(id)
-              .then(res => {
-                  context.commit('setExpense', res.data.data)
-              })
-              .catch(res => {
-                  console.log(res)
-              })
-      },
-      getExpenses({ commit }) {
-          const expenses = this.state.expense.index()
-          expenses.then(res => {
-              // console.log(res.data.data)
+    setUser(context, user) {
+      context.commit('setUser', user)
+    },
+    // retrieves the working expense
+    getExpense(context, id) {
+      return this.state.resource.show(id)
+        .then(res => {
+          context.commit('setExpense', res.data.data)
+        })
+        .catch(res => {
+          console.log(res)
+        })
+    },
+    getExpenses({ commit }) {
+      const expenses = this.state.expense.index()
+      expenses.then(res => {
+        // console.log(res.data.data)
 
-              commit('setExpenses', res.data.data)
-          })
-          .catch(res => {
-              console.log(res)
-          })
-      }
-  },
-    getters: {
-      // return the working expense
-      getExpense: (state) => state.expense
+        commit('setExpenses', res.data.data)
+      })
+        .catch(res => {
+          console.log(res)
+        })
     }
+  },
+  getters: {
+    // return the working expense
+    getExpense: (state) => state.expense,
+    getUser: (state) => state.user
+  }
 })

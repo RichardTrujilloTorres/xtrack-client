b95824c49439f4d5f789d381ae9974ab5135f3c6
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-16 00:15:55 +0200

    Add monthly summary component

---
+++ src/components/MonthlySummary.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    v-if="summary"
+    class="monthly-summary"
+  >
+    <div class="card-group">
+      <!-- highest category -->
+      <div
+        class="card"
+        style="width: 18rem;"
+      >
+        <div class="card-body">
+          <h5 class="card-title">
+            Highest Category | This Month
+          </h5>
+          <h6 class="card-subtitle mb-2 text-muted">
+            {{ summary.highestCategory.category }}
+          </h6>
+          <p class="card-text">
+            {{ summary.highestCategory.total }}
+          </p>
+        </div>
+      </div>
+
+      <!-- highest single expense -->
+      <div
+        class="card"
+        style="width: 18rem;"
+      >
+        <div class="card-body">
+          <h5 class="card-title">
+            Highest Expense | This Month
+          </h5>
+          <h6 class="card-subtitle mb-2 text-muted">
+            {{ summary.highestExpense.category }} | {{ summary.highestExpense.denomination }}
+          </h6>
+          <p class="card-text">
+            {{ summary.highestExpense.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+export default {
+  name: 'MonthlySummary',
+  data: () => ({
+    summary: null
+  }),
+  computed: {
+    ...mapActions(['getMonthlySummary'])
+  },
+  created() {
+    this.getMonthlySummary
+      .then(res => {
+        console.log(res.data.data)
+        this.summary = res.data.data
+      })
+      .catch(res => console.log(res))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>

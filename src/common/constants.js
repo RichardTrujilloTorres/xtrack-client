56ba2d918d9a6383f10caf7578436dc9e6bd4ce5
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-12-07 18:52:03 +0100

    Add constants

---
+++ src/common/constants.js
@@ -0,0 +1,15 @@
+
+const GROCERIES = {
+    key: 'groceries',
+    value: 'Groceries'
+};
+
+const OTHER = {
+    key: 'other',
+    value: 'Other'
+};
+
+export const voices = {
+    GROCERIES,
+    OTHER
+};

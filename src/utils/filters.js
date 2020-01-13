2d815816a34cc346d1bc2b38576755d0966b9afd
Richard Trujillo <richard.trujillo.torres@gmail.com>
2020-01-13 05:54:00 +0100

    Add round filter

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -2,3 +2,6 @@ import Vue from 'vue'
 
 Vue.filter('euro', (value) => `${value} €`);
 Vue.filter('currency', (value, symbol) => `${value} ${symbol}`)
+Vue.filter('round', (value) => {
+  return parseFloat(value).toFixed(2)
+});

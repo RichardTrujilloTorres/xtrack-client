df82a12133bb964e5fc3a86fd83957a26ff43108
Richard Trujillo <richard.trujillo.torres@gmail.com>
2020-01-13 05:02:51 +0100

    Add capitalization util

--- src/utils/str.js
+++ src/utils/str.js
@@ -1,10 +1,22 @@
 
 export const getValueFromStringOrJSON = (value, key) => {
-    try {
-        const category = JSON.parse(value);
-        return category[key];
-    } catch (e) {
-        return value;
-    }
-}
+  try {
+    const category = JSON.parse(value);
+    return category[key];
+  } catch (e) {
+    return value;
+  }
+};
+
+export const capitalize = (value) => {
+  if (typeof value === "undefined") {
+    return '';
+  }
+
+  const firstLetter = value[0] || value.chartAt(0);
+
+  return firstLetter
+    ? `${firstLetter.toUpperCase()}${value.slice(1)}`
+    : '';
+};
 
